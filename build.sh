0980d57a31995e6fb78be8bee798048c53d1692f
Mhmmdfas <57018840+Mhmmdfas@users.noreply.github.com>
2020-01-02 14:08:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,26 +2,23 @@
 #
 # buildbot script for compiling android ROMs using drone CI
 
-source /drone/src/config.sh
-
-ci_repo=$(cat /drone/src/.git/config | grep url | sed -e 's|url = https://github.com/||' -e 's|.git||')
-ci_url=$(echo "https://cloud.drone.io/$ci_repo/$(cat /tmp/build_no)/1/2" | tr -d " ")
+source ./config.sh
 
 # Configure git
 git config --global user.email "$GITHUB_EMAIL"
 git config --global user.name "$GITHUB_USER"
 
-#export TELEGRAM_TOKEN=$(cat /tmp/tg_token)
-#export TELEGRAM_CHAT=$(cat /tmp/tg_chat)
+export TELEGRAM_TOKEN=$(cat /tmp/tg_token)
+export TELEGRAM_CHAT=$(cat /tmp/tg_chat)
 export GITHUB_TOKEN=$(cat /tmp/gh_token)
 
-# Delete useless repos
-trim_manifest() {
+# Delete useless darwin repos
+trim_darwin() {
     cd .repo/manifests
-	sed -i -e '/darwin/d' -e '/device\/generic/d' -e '/device\/google/d' default.xml
+	sed -i '/darwin/d' default.xml
     git commit -a -m "Magic"
     cd ../
-	sed -i -e '/darwin/d' -e '/device\/generic/d' -e '/device\/google/d' manifest.xml
+	sed -i '/darwin/d' manifest.xml
     cd ../
 }
 
@@ -41,21 +38,21 @@ export outdir="out/target/product/$device"
 
 # Initialize repo
 repo init -u "$manifest_url" -b "$branch" --depth 1 &> /dev/null
-trim_manifest &> /dev/null
+trim_darwin &> /dev/null
 echo "Sync started for $manifest_url"
-#telegram -M "Sync Started for [$ROM]($manifest_url)"
+telegram -M "Sync Started for [$ROM]($manifest_url)"
 
 # Reset bash timer and begin syncing
 SECONDS=0
 if repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q &> /dev/null; then
 	# Syncing completed, clone custom repos if any
-	bash /drone/src/clone.sh
+	bash ./clone.sh
 
-	echo -e "\n\nSync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+	echo "Sync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
     echo "Build Started"
-    #telegram -M "Sync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
+    telegram -M "Sync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
 
-#Build Started: [See Progress]($ci_url)"
+Build Started: [See Progress]($ci_url)"
 	
 	# Reset bash timer and begin compilation
     SECONDS=0
@@ -65,30 +62,30 @@ if repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimiz
 
     if mka bacon | grep $device; then
 		# Build completed succesfully, upload it to github
-		export finalzip_path=$(ls "$outdir/*2019*.zip" | tail -n -1)
+		export finalzip_path=$(ls "$outdir/*2020*.zip" | tail -n -1)
 		export zip_name=$(basename -s "$finalzip_path")
-        echo -e "\n\nBuild completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
-        echo "Uploading..."
+        echo "Build completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+        echo "Uploading"
 
-        #github-release "$release_repo" "$zip_name" "master" "$ROM for $device
+        github-release "$release_repo" "$zip_name" "master" "$ROM for $device
 
-#Date: $(env TZ="$timezone" date)" "$finalzip_path"
+Date: $(env TZ="$timezone" date)" "$finalzip_path"
 
-        echo "Uploaded succesfully to $(curl -sT "$zippath" https://transfer.sh/"$zip_name".zip)"
+        echo "Uploaded"
 
-        #telegram -M "Build completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
+        telegram -M "Build completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
 
-#Download: [$zip_name](https://github.com/$release_repo/releases/download/$zip_name/"$zip_name".zip)"
+Download: [$zip_name](https://github.com/$release_repo/releases/download/$zip_name/"$zip_name".zip)"
 
     else
 		# Build failed
-        echo -e "\n\nBuild failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !!"
-        #telegram -N -M "ALERT: Build failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+        echo "ALERT: Build failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+        telegram -N -M "ALERT: Build failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
         exit 1
     fi
 else
 	# Sync failed
-    echo -e "\n\nSync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!!"
-    #telegram -N -M "ALERT: Sync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+    echo "ALERT: Sync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+    telegram -N -M "ALERT: Sync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
     exit 1
 fi
